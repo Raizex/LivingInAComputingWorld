1bd3e7eb2c5113b04a8b4fbe81ac700bc428b7b2
Justin Huffman <jwhuffman@gmail.com>
2017-10-24 15:19:09 -0600

    Initial commit.

---
+++ remainder.py
@@ -0,0 +1,11 @@
+x = 71
+y = 13
+z = 71 % 13
+print(x ,y ,z)
+
+val = int(input("Enter an integer: "))
+rem = val % 2
+if rem == 0:
+  print("Your value",val,"is an even number")
+else: 
+  print("Your value",val,"is an odd number")
---
+++ whatdoyouwant.py
@@ -0,0 +1,6 @@
+in1 = input("How many? ")
+number = int(in1)
+in2 = input("What else? ")
+num2 = in2
+total = number + num2
+print("Total is",total)
