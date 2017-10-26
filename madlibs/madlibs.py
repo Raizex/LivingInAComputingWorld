5f5ef9c7dac3851a3407d59963f3a483b5e09484
Justin Huffman <jwhuffman@gmail.com>
2017-10-26 15:16:31 -0600

    Wrote madlibs.py.

---
+++ madlibs/madlibs.py
@@ -0,0 +1,52 @@
+band_name = input("Pick a band name: ")
+
+name = input("Pick a male name (First and last): ")
+
+location = input("Pick a location: ")
+
+adjetive = input("Pick an adjetive: ")
+
+body_part_1 = input("Pick a body part: ")
+body_part_2 = input("Pick another body part: ")
+
+part_of_day_1 = input("Pick part of the day (morning, afternoon, etc...): ")
+part_of_day_2 = input("Pick another part of the day (morning, afternoon, etc...): ")
+
+noun_1 = input("Pick a noun: ")
+noun_2 = input("Pick another noun: ")
+noun_3 = input("Pick another noun: ")
+noun_4 = input("Pick another noun: ")
+
+verb_1 = input("Pick a verb (present singular form): ")
+verb_2 = input("Pick another verb (): ")
+verb_3 = input("Pick another verb (): ")
+
+print("\n\n")
+
+print(band_name,"is how the",noun_1,"feels")
+print("On the",body_part_1,"of",name)
+print("On his way home from the",location,"where he",verb_1)
+print(part_of_day_1,"s-the",adjetive,part_of_day_1,"s, don't nobody",verb_2,"right")
+print("And between the ",noun_2,"offers and the",noun_3)
+print("And if another",noun_4,"touch his",body_part_2,"trying to pull him in to",verb_3)
+print("He ain't making it to",part_of_day_2)
+
+#print("band_name:",band_name)
+#print("name:",name)
+#print("location:",location)
+#print("adjetive:",adjetive)
+
+#print("body_part_1:",body_part_1)
+#print("body_part_2:",body_part_2)
+
+#print("part_of_day_1:",part_of_day_1)
+#print("part_of_day_2:",part_of_day_2)
+
+#print("noun_1:",noun_1)
+#print("noun_2:",noun_2)
+#print("noun_3:",noun_3)
+#print("noun_4:",noun_4)
+
+#print("verb_1:",verb_1)
+#print("verb_2:",verb_2)
+#print("verb_3:",verb_3)
